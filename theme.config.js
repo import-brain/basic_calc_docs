186515a04ea8e646ec2f94b59132b21691f92000
Eric Cheng <ericcheng9316@gmail.com>
2021-11-28 17:09:59 -0500

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -4,9 +4,9 @@ export default {
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Basic_Calc</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Your Friendly Command-Line Calculator Tool
       </span>
     </>
   ),
